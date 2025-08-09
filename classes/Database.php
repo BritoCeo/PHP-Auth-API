07557263a9082e99e8698e7d8fedfd5f066e3e10
Brito Ceo <britovalerio1@gmail.com>
2025-08-10 01:24:16 +0200

    Database Config and no there no password

---
+++ classes/Database.php
@@ -0,0 +1,32 @@
+ <?php
+ 
+    class Database {
+        
+        private $db_host = 'localhost';
+        private $db_name = 'php.auth.api';
+        private $db_username = 'root';
+        private $db_password = 'Mysql344$';
+
+        public function dbConnection() {
+
+            try {
+                
+                $conn = new PDO('mysql:host='.$this->db_host.
+                ';dbname='. $this->db_name, 
+                $this->db_username,
+                $this->db_password);
+
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            } catch (PDOException $e) {
+                
+                echo "Database Connection Error". $e->getMessage();
+                exit();
+            }
+        }
+
+
+    }
+
+
+ ?>
