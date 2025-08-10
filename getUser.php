162fe24fb9bb64a3446879f39bc7d8af30fffa3a
Brito Ceo <britovalerio1@gmail.com>
2025-08-11 00:50:19 +0200

    Get User Endpoint

---
+++ getUser.php
@@ -0,0 +1,19 @@
+<?php 
+
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: acceess");
+    header("Access-Control-Allow-Methods: GET");
+    header("Content-Type: application/json; charset=UTF-8");    
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+    require __DIR__ . '/classes/Database.php';
+    require __DIR__ . '/AuthMiddleware.php';
+
+    $allheaders = getallheaders();
+    $db_connection = new Database();
+    $conn = $db_connection->dbConnection();
+    $auth = new Auth($conn, $allheaders);
+
+    echo json_encode($auth->isValid());
+
+?>
